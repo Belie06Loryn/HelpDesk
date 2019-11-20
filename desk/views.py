ff2c8a4a386241d5d08a4e469cdf9d739df9c266
Belie06Loryn <maniralexie@gmail.com>
2019-11-20 19:26:08 +0200

    UI

--- desk/views.py
+++ desk/views.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+def page(request):
+    return render(request,'all-pages/index.html',{})
+
+def learn(request):
+    return render(request,'all-pages/learn.html',{})    
